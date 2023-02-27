e41cf3d8000879ce40bb9cc6511094ade021da5e
93simon <93simonster@gmail.com>
2023-02-27 10:23:30 +0100

    check if already started

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from datetime import time, date, datetime
 import pytz
 import json
 
-
 TELE_TOKEN = os.environ.get('TELE_TOKEN')
 
 # Enable logging
@@ -14,18 +13,32 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
-
 tz_Rome = pytz.timezone('Europe/Rome')
+
+
+def check_job_exists(name: str, context: ContextTypes.DEFAULT_TYPE) -> bool:
+    """Remove job with given name. Returns whether job was removed."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    else:
+        return True
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Add a job to the queue."""
     chat_id = update.effective_message.chat_id
-    context.job_queue.run_daily(get_global_day, time(hour=8, tzinfo=tz_Rome), days=(0, 1, 2, 3, 4, 5, 6),
-                      name=str(chat_id), chat_id=chat_id)
+    if check_job_exists(str(chat_id), context):
+        next_int_day = list(context.job_queue.jobs())[0].next_t
+        next_int_day = next_int_day.strftime("%d/%m/%Y")
+        text = f"Prossima giornata mondiale: {next_int_day}"
+        await update.effective_message.reply_text(text)
+    else:
+        context.job_queue.run_daily(get_global_day, time(hour=8, tzinfo=tz_Rome), days=(0, 1, 2, 3, 4, 5, 6),
+                                    name=str(chat_id), chat_id=chat_id)
 
-    text = "Bot avviato"
-    await update.effective_message.reply_text(text)
+        text = "Bot avviato"
+        await update.effective_message.reply_text(text)
 
 
 async def get_global_day(context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -50,12 +63,10 @@ async def get_global_day(context: ContextTypes.DEFAULT_TYPE) -> None:
             await context.bot.send_message(job.chat_id, text=text)
 
 
-
 def main() -> None:
     """Run bot."""
     # Create the Application and pass it your bot's token.
     application = Application.builder().token(TELE_TOKEN).build()
-
 
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
