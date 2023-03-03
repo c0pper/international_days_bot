52af846dcf9af2fc21c59ffe36a8ddc8cc52dea3
93simon <93simonster@gmail.com>
2023-03-03 08:54:24 +0100

    next day fix

--- main.py
+++ main.py
@@ -42,6 +42,25 @@ def get_next_int_day() -> str:
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Add a job to the queue."""
+    target_dates = []
+    with open("db.json", "r", encoding="utf8") as db:
+        data = json.load(db)
+        giorni = data.keys()
+        print(giorni)
+        for g in giorni:
+            datetime_object = datetime.strptime(g, '%d %B')
+            day, month = datetime_object.day, datetime_object.month
+            target_date = date(date.today().year, month, day)
+            target_dates.append(target_date)
+
+    for d in target_dates:
+        print(d, date.today(), "\n")
+        if date.today() == d:
+            datetime_object = datetime.fromisoformat(str(date.today()))
+            data_formattata = datetime_object.strftime('%#d %B')
+            print(data_formattata)
+
+
     chat_id = update.effective_message.chat_id
     next_int_day = get_next_int_day()
     if check_job_exists(str(chat_id), context):
