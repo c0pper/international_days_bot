5a4f21dadd7f7f43783eb87e6ba6821cb3b0b8b6
93simon <93simonster@gmail.com>
2023-03-03 08:50:32 +0100

    next day fix

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         text = f"Prossima giornata mondiale: {next_int_day}"
         await update.effective_message.reply_text(text)
     else:
-        context.job_queue.run_daily(get_global_day, time(hour=8, minute=43, tzinfo=tz_Rome), days=(0, 1, 2, 3, 4, 5, 6),
+        context.job_queue.run_daily(get_global_day, time(hour=8, minute=52, tzinfo=tz_Rome), days=(0, 1, 2, 3, 4, 5, 6),
                                     name=str(chat_id), chat_id=chat_id)
 
         text = f"Bot avviato. Prossima giornata mondiale: {next_int_day}"
